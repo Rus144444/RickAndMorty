22e77b1f9156efb0f4f6f45b56b3cbebe685698d
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-20 09:34:37 +0200

    refactor CharactersPage add axios to CharactersPage and pars array

--- src/pages/CharactersPage/CharactersPage.jsx
+++ src/pages/CharactersPage/CharactersPage.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
+import { useEffect } from "react"
+import axios from "axios"
 import s from "./CharactersPage.module.css"
 
+ 
 export const CharactersPage = () => {
-    return (
-        <div className={s.title}>CharactersPage</div>
-    )
+  const [characters, setCharacters] = useState([])
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character").then((res) => {
+      setCharacters(res.data.results)
+    })
+  }, [])
+ 
+  return (
+    <div className="pageContainer">
+      <h1 className={"pageTitle"}>CharacterPage</h1>
+      <ul className={s.characters}>
+       {characters.length && ( characters.map((character) => {
+        return(<div key={character.id} className={s.character}>
+          <div className={s.characterLink}>{character.name}</div>
+          <img src={character.image} alt={`${character.name} avatar`} />
+        </div>)
+       })
+      )}
+      </ul>
+    </div>
+  )
 }
