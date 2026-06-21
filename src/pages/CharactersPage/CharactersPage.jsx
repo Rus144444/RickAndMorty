e1e55ad8304cf023a6836e28aabbf2a6e3f32068
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-21 15:02:05 +0200

    refactor charecter

--- src/pages/CharactersPage/CharactersPage.jsx
+++ src/pages/CharactersPage/CharactersPage.jsx
@@ -7,42 +7,40 @@ import s from "./CharactersPage.module.css"
  
 export const CharactersPage = () => {
     const [characters, setCharacters] = useState([])
+    const [error, setError] = useState(null)
     const [info, setInfo] = useState({
         count: 0,
         pages: 0,
         next: null,
         prev: null,
     })
-
-    const [error, setError] = useState(null)
  
-  const fetchData = (url) => {
-    axios.get(url).then((res) => {
-      setCharacters(res.data.results)
-      setInfo(res.data.info)
-    })
-  }
+    const fetchData = (url) => {
+        axios.get(url).then((res) => {
+        setCharacters(res.data.results)
+        setInfo(res.data.info)
+        })
+    }
  
+    const nextPageHandler = () => {
+        fetchData(info.next)
+    }
+ 
+    const previousPageHandler = () => {
+        fetchData(info.prev)
+    }
 
-  useEffect(() => {
-    axios.get("https://rickandmortyapi.com/api/character").then((res) => {
-      setCharacters(res.data.results)
-      setInfo(res.data.info)
-    })
-  }, [])
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/character").then((res) => {
+        setCharacters(res.data.results)
+        setInfo(res.data.info)
+        })
+    }, [])
 
     const searchHandler = (event) => {
         const value = event.currentTarget.value
         fetchData(`https://rickandmortyapi.com/api/character?name=${value}`)
     }
-
-  const nextPageHandler = () => {
-    fetchData(info.next)
-  }
- 
-  const previousPageHandler = () => {
-    fetchData(info.prev)
-  }
  
   return (
     <div className="pageContainer">
