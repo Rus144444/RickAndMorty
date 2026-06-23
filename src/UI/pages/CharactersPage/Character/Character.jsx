e6b935e76d0eec9837e4c92415aa7fa69dc6cb1a
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-23 07:45:08 +0200

    seporet logic Character.jsx

--- src/UI/pages/CharactersPage/Character/Character.jsx
+++ src/UI/pages/CharactersPage/Character/Character.jsx
@@ -3,12 +3,8 @@ import axios from "axios";
 import { Link, useParams } from "react-router";
 import s from "./Charecter.module.css";
 
-// const useCharacter = () => {
-
-// }
-
-export const Character = () => {
-    const { id } = useParams();
+const useCharacter = () => {
+  const { id } = useParams();
     const [character, setCharacter] = useState(null);
 
     useEffect(() => {
@@ -21,7 +17,11 @@ export const Character = () => {
                 console.error(err);
             });
     }, [id]);
+    return {character}
+}
 
+export const Character = () => {
+    const [character] = useCharacter()
     if (!character) {
         return (
             <div className={s.pageContainer}>
@@ -29,73 +29,58 @@ export const Character = () => {
             </div>
         );
     }
-
     return (
         <div className={s.pageContainer}>
             <div className={s.container}>
                 <h1 className={s.pageTitle}>
                     {character.name}
                 </h1>
-
-                <div className={s.content}>
-                    <img
-                        className={s.img}
-                        src={character.image}
-                        alt={character.name}
+            <div className={s.content}>
+                <img
+                    className={s.img}
+                    src={character.image}
+                    alt={character.name}
+                />
+            <div className={s.description}>
+                <div className={s.statusContainer}>
+                    <span
+                        className={`${s.statusDot} ${
+                            character.status === "Alive"
+                                ? s.alive
+                                : character.status === "Dead"
+                                ? s.dead
+                                : s.unknown
+                        }`}
                     />
-
-                    <div className={s.description}>
-                        <div className={s.statusContainer}>
-                            <span
-                                className={`${s.statusDot} ${
-                                    character.status === "Alive"
-                                        ? s.alive
-                                        : character.status === "Dead"
-                                        ? s.dead
-                                        : s.unknown
-                                }`}
-                            />
-
-                            {character.status} — {character.species}
-                        </div>
-
-                        <div className={s.info}>
-                            <p className={s.subTitle}>Gender</p>
-                            <p className={s.subTitleResult}>
-                                {character.gender}
-                            </p>
-                        </div>
-
-                        <div className={s.info}>
-                            <p className={s.subTitle}>Origin</p>
-                            <p className={s.subTitleResult}>
-                                {character.origin.name}
-                            </p>
-                        </div>
-
-                        <div className={s.info}>
-                            <p className={s.subTitle}>Last known location</p>
-                            <p className={s.subTitleResult}>
-                                {character.location.name}
-                            </p>
-                        </div>
-
-                        <div className={s.info}>
-                            <p className={s.subTitle}>Episode count</p>
-                            <p className={s.subTitleResult}>
-                                {character.episode.length}
-                            </p>
-                        </div>
-                    </div>
+                    {character.status} — {character.species}
                 </div>
-
-                <Link
-                    to="/RickAndMorty/characters"
-                    className={s.backButton}
-                >
-                    ← Go Back
-                </Link>
+                <div className={s.info}>
+                    <p className={s.subTitle}>Gender</p>
+                    <p className={s.subTitleResult}>
+                        {character.gender}
+                    </p>
+                </div>
+                <div className={s.info}>
+                    <p className={s.subTitle}>Origin</p>
+                    <p className={s.subTitleResult}>
+                        {character.origin.name}
+                    </p>
+                </div>
+                <div className={s.info}>
+                    <p className={s.subTitle}>Last known location</p>
+                    <p className={s.subTitleResult}>
+                        {character.location.name}
+                    </p>
+                </div>
+                <div className={s.info}>
+                    <p className={s.subTitle}>Episode count</p>
+                    <p className={s.subTitleResult}>
+                        {character.episode.length}
+                    </p>
+                </div>
             </div>
         </div>
-    );
-};
+            <Link to="/RickAndMorty/characters" className={s.backButton}> ← Go Back</Link>
+        </div>
+    </div>
+)}
